db64ee62fcf145ede1bdd9058185b87b7dbc802f
hajee <hajee@moretIA.com>
2014-01-27 11:47:11 +0100

    Added Oracle_service type
    Signed-off-by: hajee <hajee@moretIA.com>

---
+++ lib/puppet/type/oracle_service.rb
@@ -0,0 +1,53 @@
+require 'easy_type'
+require 'utils/oracle_access'
+
+module Puppet
+  newtype(:oracle_service) do
+    include EasyType
+    include Utils::OracleAccess
+
+    desc %q{
+      This resource allows you to manage a service in an Oracle database.
+    }
+
+    ensurable
+
+    set_command(:sql)
+
+
+    to_get_raw_resources do
+      sql "select name from dba_services"
+    end
+
+    on_create do
+      debugger
+      name = to_resource[:name]
+      new_services = current_services << name
+      set_services_command(new_services)
+    end
+
+    on_modify do
+      fail "It shouldn't be possible to modify a service"
+    end
+
+    on_destroy do
+      debugger
+      name = to_resource[:name]
+      new_services = current_services.delete_if {|e| e == name }
+      set_services_command(new_services)
+    end
+
+    parameter :name
+
+    private
+
+      def set_services_command(services)
+        "alter system set service_names = '#{services.join(',')}'"        
+      end
+
+      def current_services
+        provider.class.instances.map(&:name)
+      end
+
+  end
+end
