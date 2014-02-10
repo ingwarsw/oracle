79f218c7be1053115bbc87b9df44d01274adb6dc
hajee <hajee@moretIA.com>
2014-02-10 14:21:22 +0100

    Don't change case of service name
    Signed-off-by: hajee <hajee@moretIA.com>

--- lib/puppet/type/oracle_service/name.rb
+++ lib/puppet/type/oracle_service/name.rb
@@ -1,10 +1,10 @@
 newparam(:name) do
   include EasyType
-  include EasyType::Mungers::Upcase
 
   desc "The service name"
 
   isnamevar
+
 
   to_translate_to_resource do | raw_resource|
     raw_resource.column_data('NAME').upcase
