f30991749063bc81b175ad6e97d7a3c05740028c
hajee <hajee@moretIA.com>
2014-02-17 16:24:56 +0100

    Added basic tests for oracle_service
    Signed-off-by: hajee <hajee@moretIA.com>

--- lib/puppet/type/oracle_service/name.rb
+++ lib/puppet/type/oracle_service/name.rb
@@ -5,10 +5,8 @@ newparam(:name) do
 
   isnamevar
 
-
   to_translate_to_resource do | raw_resource|
-    raw_resource.column_data('NAME').upcase
+    raw_resource.column_data('NAME')
   end
-
 
 end
