a9d4f111da5d86dda75430e9eb2c4304bb3c6c1f
Bert Hajee <hajee@moretIA.com>
2014-03-06 21:33:35 +0100

    Cleanup some old stuff
    Signed-off-by: Bert Hajee <hajee@moretIA.com>

--- manifests/config.pp
+++
@@ -1,6 +0,0 @@
-# == Class oracle::config
-#
-# This class is called from oracle
-#
-class oracle::config {
-}
--- manifests/install.pp
+++
@@ -1,9 +0,0 @@
-# == Class oracle::install
-#
-class oracle::install {
-  include oracle::params
-
-  package { $oracle::params::package_name:
-    ensure => present,
-  }
-}
